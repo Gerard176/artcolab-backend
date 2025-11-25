96f0864dcce0add1ce07513ead179a06be4fbd93
Gerardo Duarte <gaduarter@ufpso.edu.co>
2025-11-25 10:30:44 -0500

    actualizacion reciente

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BASE_URL } from './config/config.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -20,6 +20,6 @@ app.use('/uploads', express.static("public/img"));
 app.use('/publicaciones', express.static("public/img/publicaciones"));
 
 // 🔹 Escucha en todas las interfaces
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`✅ Servidor corriendo en 127.0.0.1:${PORT}`);
+app.listen(PORT, '192.168.170.84', () => {
+  console.log(`✅ Servidor corriendo en 192.168.170.84:${PORT}`);
 });
